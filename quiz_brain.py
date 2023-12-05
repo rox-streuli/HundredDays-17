6560aee945c54b137a228345aa660ee8fd7fb568
rox-streuli <roxstreuli@gmail.com>
2023-12-05 21:54:58 +0000

    Add quiz_brain.py

---
+++ quiz_brain.py
@@ -0,0 +1,13 @@
+class QuizBrain:
+
+    def __init__(self, q_list):
+        self.q_num = 0
+        self.question_bank = q_list
+
+    def next_question(self):
+        """Retrieve the item at the current question number from the
+        question list. And once you have this item, use the input function to
+        show the user the question text and ask for the
+        """
+
+    pass
