9f85e0f7ec546893b51c09d39cad2cfbc504d19a
rox-streuli <roxstreuli@gmail.com>
2023-12-16 17:08:55 +0000

    Adapted quiz game to accept new list of questions.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from question_model import Question
-from data import question_data
+from data import question_data, question_data_computers
 from quiz_brain import QuizBrain
 
 question_bank = []
 
-for item in question_data:
-    question_bank.append(Question(q_text=item['text'], q_answer=item['answer']))
+# for item in question_data:
+#     question_bank.append(Question(q_text=item['text'], q_answer=item['answer']))
+#
+# quiz = QuizBrain(question_bank)
+
+for item in question_data_computers:
+    question_bank.append(Question(q_text=item['question'], q_answer=item[
+        'correct_answer']))
 
 quiz = QuizBrain(question_bank)
 
