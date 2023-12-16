19fd5e28d1f98c07f64c47d4d493191691bfd523
rox-streuli <roxstreuli@gmail.com>
2023-12-16 15:48:39 +0000

    Add Methods. still_has_question() next_question() check_answer() Extended next_question()

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for item in question_data:
     question_bank.append(Question(q_text=item['text'], q_answer=item['answer']))
 
-# print(question_bank)
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You have completed the quiz!")
+print(f"your final score was: {quiz.score}/{quiz.q_num}")
--- quiz_brain.py
+++ quiz_brain.py
@@ -3,11 +3,27 @@ class QuizBrain:
     def __init__(self, q_list):
         self.q_num = 0
         self.question_bank = q_list
+        self.score = 0
+
+    def still_has_question(self):
+        return self.q_num < len(self.question_bank)
 
     def next_question(self):
-        """Retrieve the item at the current question number from the
-        question list. And once you have this item, use the input function to
-        show the user the question text and ask for the
+        """Retrieve item at the current question number from the
+        question list.
+        Use the input function to show the user the question text
+        and ask for their choice.
         """
+        current_question = self.question_bank[self.q_num]
+        self.q_num += 1
+        user_answer = input(f"Q.{self.q_num}: {current_question.text}"
+                            f"(True/False): ")
+        self.check_answer(user_answer, current_question.answer)
 
-    pass
+    def check_answer(self, user_choice, correct_answer):
+        if user_choice.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print(f"That is wrong. The correct answer was: {correct_answer}")
+        print(f"Your current score is: {self.score}/{self.q_num}\n")
